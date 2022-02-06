96790b1bc7d924ccaad8c892caf59104d852295e
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-02-06 10:19:39 -0600

    trash box disappears and buttons are styled

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,15 +82,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover");
+    $('.bottom-trash').removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log(event);
+    $(this).addClass("dropover-active");
   },
   out: function(event) {
     console.log(event);
+    $(this).removeClass("dropover-active");
   },
   update: function() {
     var tempArr = [];
@@ -136,9 +142,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -161,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
